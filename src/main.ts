20596680d9766aaf381f885205a96bc139e62b2a
Xing <xzhong15@ucsc.edu>
2023-10-17 23:24:01 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Xing's game";
+const gameName = "Star Studio";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
-header.innerHTML = gameName;
+header.innerHTML = `
+  <div class="card"> 
+    <span>${gameName}</span>
+    <div>-Piggy Bank<div>
+  </div>
+`;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "👛";
+button.style.fontSize = "50px";
+app.append(button);
